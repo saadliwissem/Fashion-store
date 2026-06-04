148f315b4b67f0103de74b571f022ca0b06d075b
Kadhem SELLAMI <kadhemsellami1@gmail.com>
2026-06-04 10:42:04 +0100

    Upgraded vuln. packs. and changed 'react-helmet' to 'react-helmet-async'.

--- src/pages/ChronicleDetailPage.jsx
+++ src/pages/ChronicleDetailPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import {
   ArrowLeft,
   Share2,
--- src/pages/EnigmaDetailPage.jsx
+++ src/pages/EnigmaDetailPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import {
   ArrowLeft,
   Share2,
